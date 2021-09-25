b292cf5b3edad34ae210cc2f8657f229d44a8bb6
titepasile <89131385+titepasile@users.noreply.github.com>
2021-09-25 21:02:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace EigenesProjekt_Petit_SiriguElisa
 {
@@ -10,113 +10,122 @@ namespace EigenesProjekt_Petit_SiriguElisa
             Random random = new Random();
             int secretNumber = random.Next(1, 100);
 
-            Console.WriteLine("Wählen Sie eine Sprache / Choose a language: ");
-            Console.WriteLine("Für Deustch wählen Sie die 1 und klicken auf Enter / For English write 2 and press Enter");
-            Console.WriteLine("---------------------------------------------------------------");
-
-            string selection = Console.ReadLine();
-            int z = Int32.Parse(selection);
-
-            if (z == 1)
+            try
             {
-                Console.WriteLine("Herzlich willkommen zu meinem ersten, selbst programmierten Minigame. Ihre Aufgabe ist es, die Geheimzahl zu erraten.");
-                Console.WriteLine("Überlegen Sie sich eine Zahl zwischen 1 und 100.");
-                Console.WriteLine("-------------------------------------------------------------");
-                Console.Write("Schreiben Sie hier Ihre Zahl und drücken Sie dann die Entertaste: ");
-                string userinput = Console.ReadLine();
-                int x = Int32.Parse(userinput);
 
-                int attemps = 0;
+                Console.WriteLine("Wählen Sie eine Sprache / Choose a language: ");
+                Console.WriteLine("Für Deustch wählen Sie die 1 und klicken auf Enter / For English write 2 and press Enter");
 
-                if (x < 1 || x > 100)
+                string selection = Console.ReadLine();
+                int z = Int32.Parse(selection);
+
+                if (z == 1)
                 {
-                    Console.WriteLine("--------------------------------------------------------------------");
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Ungültige Eingabe, bitte erneut um eine Zahl zwischen 1 und 100."); 
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
+                    Console.WriteLine("---------------------------------------------------------------");
+                    Console.WriteLine("Herzlich willkommen zu meinem ersten, selbst programmierten Minigame. Ihre Aufgabe ist es, die Geheimzahl zu erraten.");
+                    Console.WriteLine("Überlegen Sie sich eine Zahl zwischen 1 und 100.");
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.Write("Schreiben Sie hier Ihre Zahl und drücken Sie dann die Entertaste: ");
+                    string userinput = Console.ReadLine();
+                    int x = Int32.Parse(userinput);
 
-                while (x != secretNumber)
-                {
-                    if (x < secretNumber)
-                    {
-                        Console.WriteLine("Ihre Zahl ist zu klein.");
-                        Console.WriteLine("Versuchen Sie es noch einmal:");
-                        Console.WriteLine("-------------------------------------------------------------");
+                    int attemps = 0;
 
-                    }
-                    else if (x > secretNumber)
+                    if (x < 1 || x > 100)
                     {
-                        Console.WriteLine("Ihre Zahl ist zu gross");
-                        Console.WriteLine("Versuchen Sie es noch einmal:");
-                        Console.WriteLine("-------------------------------------------------------------");
+                        Console.WriteLine("--------------------------------------------------------------------");
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Ungültige Eingabe, bitte erneut um eine Zahl zwischen 1 und 100.");
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
 
-                    x = Int32.Parse(Console.ReadLine());
-                    attemps = attemps + 1;
+                    while (x != secretNumber)
+                    {
+                        if (x < secretNumber)
+                        {
+                            Console.WriteLine("Ihre Zahl ist zu klein.");
+                            Console.WriteLine("Versuchen Sie es noch einmal:");
+                            Console.WriteLine("-------------------------------------------------------------");
+
+                        }
+                        else if (x > secretNumber)
+                        {
+                            Console.WriteLine("Ihre Zahl ist zu gross");
+                            Console.WriteLine("Versuchen Sie es noch einmal:");
+                            Console.WriteLine("-------------------------------------------------------------");
+                        }
+
+                        x = Int32.Parse(Console.ReadLine());
+                        attemps = attemps + 1;
+                    }
+                    
+                    if (x == secretNumber)
+                    {
+                        Console.Clear();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(x);
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        Console.WriteLine("Super! Sie haben die Geheimzahl erraten.");
+                        Console.WriteLine("Drücken Sie Enter, um Ihre Rateversuche anzeigen zu lassen.");
+                        Console.WriteLine("Sie haben " + attemps + " Versuche gebraucht.");
+                    }
                 }
-
-                if (x == secretNumber)
+                if (z == 2)
                 {
-                    Console.Clear();
+                    Console.WriteLine("Welcome too my minigame called TheSecretNumber. Your yob is, to guess the secret number. Good luck ;)");
+                    Console.WriteLine("Write a number between 1 an 100");
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.Write("Write your number right here and press Enter: ");
+                    string userinput = Console.ReadLine();
+                    int x = Int32.Parse(userinput);
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(x);
-                    Console.ForegroundColor = ConsoleColor.White;
+                    int attempts = 0;
 
-                    Console.WriteLine("Super! Sie haben die Geheimzahl erraten.");
-                    Console.WriteLine("Drücken Sie Enter, um Ihre Rateversuche anzeigen zu lassen.");
-                    Console.ReadKey();
-                    Console.WriteLine("Sie haben " + attemps + " Versuche gebraucht.");
+                    if (x < 1 || x > 100)
+                    {
+                        Console.WriteLine("Wrong entry! Please write a number between 1 and 100.");
+                    }
+
+                    while (x != secretNumber)
+                    {
+                        if (x < secretNumber)
+                        {
+                            Console.WriteLine("Your number is too small.");
+                            Console.WriteLine("Try again:");
+                            Console.WriteLine("-------------------------------------------------------------");
+                        }
+                        else if (x > secretNumber)
+                        {
+                            Console.WriteLine("Your number is too big.");
+                            Console.WriteLine("Try again:");
+                            Console.WriteLine("-------------------------------------------------------------");
+                        }
+                        x = Int32.Parse(Console.ReadLine());
+                        attempts = attempts + 1;
+                    }
+
+                    if (x == secretNumber)
+                    {
+                        Console.Clear();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(x);
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        Console.WriteLine("Awesome! You guessed the right number.");
+                        Console.WriteLine("Press Enter to show your attempts");
+                        Console.WriteLine("It took you " + attempts + " attempts to guess the secret number.");
+                    }
                 }
             }
-            else if (z == 2)
+            catch (Exception)
             {
-                Console.WriteLine("Welcome too my minigame called TheSecretNumber. Your yob is, to guess the secret number. Good luck ;)");
-                Console.WriteLine("Write a number between 1 an 100");
-                Console.WriteLine("-------------------------------------------------------------");
-                Console.Write("Write your number right here and press Enter: ");
-                string userinput = Console.ReadLine();
-                int x = Int32.Parse(userinput);
-
-                int attempts = 0;
-
-                if (x < 1 || x > 100)
-                {
-                    Console.WriteLine("Wrong entry! Please write a number between 1 and 100.");
-                }
-
-                while (x != secretNumber)
-                {
-                    if (x < secretNumber)
-                    {
-                        Console.WriteLine("Your number is too small.");
-                        Console.WriteLine("Try again:");
-                        Console.WriteLine("-------------------------------------------------------------");
-                    }
-                    else if (x > secretNumber)
-                    {
-                        Console.WriteLine("Your number is too big.");
-                        Console.WriteLine("Try again:");
-                        Console.WriteLine("-------------------------------------------------------------");
-                    }
-                    x = Int32.Parse(Console.ReadLine());
-                    attempts = attempts + 1;
-                }
-
-                if (x == secretNumber)
-                {
-                    Console.Clear();
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(x);
-                    Console.ForegroundColor = ConsoleColor.White;
-                    
-                    Console.WriteLine("Awesome! You guessed the right number.");
-                    Console.WriteLine("Press Enter to show your attempts");
-                    Console.ReadKey();
-                    Console.WriteLine("It took you " + attempts + " attempts to guess the secret number.");
-                }
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("ERROR. Geben Sie bitte eine Zahl ein!");
+                Console.WriteLine("Write a number!");
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
     }
